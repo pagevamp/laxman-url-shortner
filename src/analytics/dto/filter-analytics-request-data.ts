2efeb2902f8aa49f4b4baa6b868ae96eff9b52e6
Laxman Rumba <rumbavai767@gmail.com>
2025-11-03 14:37:26 +0545

    modified transform dto

--- src/analytics/dto/filter-analytics-request-data.ts
+++ src/analytics/dto/filter-analytics-request-data.ts
@@ -25,15 +25,23 @@ export class FilterAnalyticsRequestData {
 
   @IsOptional()
   @IsDateString()
-  @Transform(({ value }) => new Date(value).toUTCString(), {
-    toPlainOnly: true,
-  })
+  @Transform(
+    ({ value }: { value: string }) => {
+      if (!value) return null;
+      return new Date(value).toUTCString();
+    },
+    { toPlainOnly: true },
+  )
   startDate?: Date | null;
 
   @IsOptional()
   @IsDateString()
-  @Transform(({ value }) => new Date(value).toUTCString(), {
-    toPlainOnly: true,
-  })
+  @Transform(
+    ({ value }: { value: string }) => {
+      if (!value) return null;
+      return new Date(value).toUTCString();
+    },
+    { toPlainOnly: true },
+  )
   endDate?: Date | null;
 }
