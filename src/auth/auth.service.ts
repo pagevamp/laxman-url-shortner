dc671d3ef8020d654f3f52e55151678fd82df5e8
Laxman Rumba <rumbavai767@gmail.com>
2025-11-04 10:48:14 +0545

    fix(auth): modified to delete the old verification email

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -81,6 +81,10 @@ export class AuthService {
       throw new BadRequestException('User is already verified');
     }
 
+    await this.emailVerificationRepo.delete({
+      userId: user.id,
+    });
+
     const payload: EmailVerificationPayload = {
       email,
     };
