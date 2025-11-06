ff4eb29d42e713041db2812f0286f5fa8ec81501
Laxman Rumba <rumbavai767@gmail.com>
2025-11-06 07:47:04 +0545

    feat(mail): changed host from gmail to mailtrap

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -2,28 +2,38 @@ import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { createTransport } from 'nodemailer';
 import Mail from 'nodemailer/lib/mailer';
 import { SendMailRequestData } from './dto/send-mail-request-data';
+
 @Injectable()
 export class EmailService {
   private nodemailerTransport: Mail;
+
   constructor() {
     this.nodemailerTransport = createTransport({
-      host: 'smtp.gmail.com',
-      port: 587,
-      secure: false,
+      host: process.env.MAILTRAP_HOST,
+      port: 2525,
       auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
+        user: process.env.MAILTRAP_USER,
+        pass: process.env.MAILTRAP_PASSWORD_DEMO,
       },
     });
   }
 
-  async sendMail(sendMailRequestdata: SendMailRequestData) {
+  async sendMail(sendMailRequestData: SendMailRequestData) {
     try {
-      await this.nodemailerTransport.sendMail(sendMailRequestdata);
+      await this.nodemailerTransport.sendMail({
+        from: {
+          name: 'URL Shortener',
+          address: process.env.MAILTRAP_DOMAIN__ADDRESS!,
+        },
+        to: sendMailRequestData.to,
+        subject: sendMailRequestData.subject,
+        text: sendMailRequestData.text,
+      });
     } catch (error: unknown) {
-      const message = error instanceof Error ? error.message : 'Unknown error';
+      const message =
+        error instanceof Error ? error.message : 'Unknown error occurred';
       throw new InternalServerErrorException(
-        'Failed to send email: ' + message,
+        `Failed to send email: ${message}`,
       );
     }
   }
