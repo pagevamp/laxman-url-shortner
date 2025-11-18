ba542ab5c4062ea20adeba3e6ae56278e02107f6
Laxman Rumba <rumbavai767@gmail.com>
2025-11-18 23:57:53 +0545

    refact/ added CORS for frontend

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import { RateLimitMiddleware } from './middleware/rate-limit-midddleware';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['http://localhost:3000'], // frontend URLs allowed
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true, // if you need cookies/auth headers
+  });
   app.use(new RateLimitMiddleware().use);
   app.useGlobalPipes(
     new ValidationPipe({
